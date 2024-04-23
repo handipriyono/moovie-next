35dda82885ab8a6877c4d1231a552d8403911d84
Handi Priyono <handimacbook@gmail.com>
2024-04-23 23:33:14 +0700

    feat: ini lib

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
